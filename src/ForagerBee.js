8ac0dedb4912a0b54a88284e8c6e126568cb8c0b
Anton V Shtylman <AntonVS.Professional@gmail.com>
2017-10-30 01:15:06 -0700

    Completed the 6ees6ees6ees project.

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -3,5 +3,11 @@ class ForagerBee extends Bee {
     super();
     this.age = 10;
     this.job = 'find pollen';
+    this.canFly = true;
+    this.treasureChest = [];
+  }
+
+  forage(treasure) {
+    this.treasureChest.push(treasure);
   }
 };
